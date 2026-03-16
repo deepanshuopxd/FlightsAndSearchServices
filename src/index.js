e2064841a44f479d19443e028bbdef881a55872e
deepanshuopxd <deepanshufulara17@gmail.com>
2026-03-16 17:32:25 +0530

    dotenv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+// require('dotenv').config();
+const {PORT} = require('./config/serverConfig')
 
 const SetupAndStartService = async ()=>{
     const PORT = 3000;
     const app  = express();
     app.listen(PORT,()=>{
         console.log(`The server is started and running on ${PORT}`);
-    })
+        // console.log(process.env);
+    });
 }
 SetupAndStartService();
