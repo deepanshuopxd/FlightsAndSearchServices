4a794b10903b4e85cd6dad0266ab49ea93e236c2
deepanshuopxd <deepanshufulara17@gmail.com>
2026-03-19 11:56:39 +0530

    city repo

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ const express = require('express');
 const bodyParser  = require('body-parser');
 // require('dotenv').config();
 const {PORT} = require('./config/serverConfig')
-
+const {City} = require('./models/index');
+const CityRepository = require('./repository/city-repository');
 const SetupAndStartService = async ()=>{
     // const PORT = 3000;
     const app  = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-    app.listen(PORT,()=>{
+    app.listen(PORT,async ()=>{
         console.log(`The server is started and running on ${PORT}`);
         // console.log(process.env);
-    });
+        // await City.create({
+        //     name:"New Delhi",
+        // })
+
+        const repo = new CityRepository();
+        // repo.createCity({name:"Mumbai"});
+        // repo.createCity({name:"New Delhi"});
+        // repo.createCity({name:"Lucknow"});
+        // repo.deleteCity({cityId:6});
+    }); 
 }
 SetupAndStartService();
