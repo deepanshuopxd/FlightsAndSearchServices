a567e873ffbe6a22d0727145971233fe11487313
deepanshuopxd <deepanshufulara17@gmail.com>
2026-03-30 19:59:05 +0530

    airport model added

---
+++ src/seeders/20260320135040-add-airports.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     * 
+    */
+    await queryInterface.bulkInsert('Airports', [
+    {
+      name: 'Kempegowde International Airport',
+      cityId: 2,
+      createdAt: new Date(),
+      updatedAt : new Date()
+
+    },
+    {
+      name: 'Mysuru Airport',
+      cityId: 2,
+      createdAt: new Date(),
+      updatedAt : new Date()
+    },
+    {
+      name: 'Mengaluru International Airport',
+      cityId: 2,
+      createdAt: new Date(),
+      updatedAt : new Date()
+    },
+    {
+      name: 'Indira Gandhi International Airport',
+      cityId: 16,
+      createdAt: new Date(),
+      updatedAt : new Date()
+    },
+    {
+      name: 'Chaudhry Charan Singh International Airport',
+      cityId: 11,
+      createdAt: new Date(),
+      updatedAt : new Date()
+    }
+  ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
